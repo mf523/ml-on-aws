b0d38339407c38617dcd3ac69bd84e7461cc4970
Laurence MIAO <mf523@outlook.com>
2020-05-21 16:29:29 +0000

    mainly changed pgan.ipynb

--- byos-pytorch-gan/pgan/model_tools.py
+++ byos-pytorch-gan/pgan/model_tools.py
@@ -1,3 +1,24 @@
+# generate a list of random noises
+def generate_noises(num_images=1, dim=512):
+    import torch
+    
+    noises = torch.randn(num_images, dim)
+    
+    return noises
+
+# plot images using torchvision and matplotlib
+def show_multiple_pictures(pics):
+    import matplotlib.pyplot as plt
+    import torchvision
+    import torch
+
+    generated_images = torch.Tensor(pics)
+    grid = torchvision.utils.make_grid(generated_images.clamp(min=-1, max=1),
+                                       scale_each=True, normalize=True)
+    plt.imshow(grid.permute(1, 2, 0).cpu().numpy())
+    plt.show()
+    
+    
 def create_pgan_netG_512_512(gnet_cls, model_pt_path=None, device='cpu'):
     import torch
     
